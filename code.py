f32531c6b9a0f94e013e0d9492e7f923ca298ec3
Gregory Melsby <greg27426@gmail.com>
2024-04-15 18:00:56 -0400

    midi expression pedal works, tuned values for my footswitch 0-127

---
+++ code.py
@@ -0,0 +1,50 @@
+# SPDX-FileCopyrightText: 2022 Liz Clark for Adafruit Industries
+# SPDX-License-Identifier: MIT
+
+import time
+import board
+import usb_midi
+import adafruit_midi
+import simpleio
+from analogio import AnalogIn
+from adafruit_midi.control_change import ControlChange
+
+# change in modulation value necessary to trigger midi change
+SENSITIVITY = 2
+
+#  midi setup
+midi = adafruit_midi.MIDI(
+    midi_in=usb_midi.ports[0], in_channel=0, midi_out=usb_midi.ports[1], out_channel=1
+)
+
+#  potentiometer setup
+mod_pot = AnalogIn(board.A0)
+
+#  function to read analog input
+def val(pin):
+    return pin.value
+
+#  variables for last read value
+#  defaults to 0
+mod_val2 = 0
+
+while True:
+
+    #  Print out the min/max values from potentiometer to the serial monitor and plotter
+    #print((mod_pot.value,))
+    time.sleep(0.025)
+
+    #  map range of potentiometer input to midi values - update the min/max values below
+    mod_val1 = round(simpleio.map_range(val(mod_pot), 400, 65535, 0, 127))
+
+    #  if modulation value is updated...
+    if abs(mod_val1 - mod_val2) >= 2:
+        #  update mod_val2
+        mod_val2 = mod_val1
+        #  create integer
+        modulation = int(mod_val2)
+        #  create CC message
+        modWheel = ControlChange(1, modulation)
+        #  send CC message
+        print(modWheel)
+        midi.send(modWheel)
